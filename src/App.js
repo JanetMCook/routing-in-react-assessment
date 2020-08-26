17778c76ce9e16770da319234299d4fbfad804b7
Janet Cook <monickercoding@gmail.com>
2020-08-26 13:59:46 -0600

    Initial commit for assessment

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+
+import Welcome from "./components/welcome/Welcome";
+import Clock from "./components/clock/Clock";
+import Contact from "./components/contact/Contact";
+import Navigation from "./components/navigation/Navigation";
+import NotFound from "./components/notfound/NotFound";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+      <div className="App">
+
+        <Navigation /> 
+
+      <Switch>     
+        <Route
+          exact
+          path="/"
+          render={(props) => <Welcome {...props} name="janet" />}
+        />
+
+        <Route
+          path="/welcome/:name"
+          render={(props) => <Welcome {...props} name={props.match.params.name} />}
+        />
+
+        <Route 
+          exact
+          path="/clock" 
+          component={Clock} 
+        />
+
+        <Route
+          exact 
+          path="/contact" 
+          component={Contact} 
+        />
+
+        <Route
+          component={NotFound}
+        />
+
+      </Switch>
+      
+      </div>
+    );
+  }
 
 export default App;
---
+++ src/components/notfound/NotFound.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function NotFound (props) {
+    return <div className="NotFound">Page Not Found </div>;
+
+}
+export default NotFound;
